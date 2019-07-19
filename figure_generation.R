46c015b419e51c5774d4adba25ff5919005cce32
Sam Thompson <thompsonsed@gmail.com>
2019-07-19 09:41:38 +0100

    Fixed display of speciation rates

--- figure_generation.R
+++ figure_generation.R
@@ -9,7 +9,6 @@ library(ggpubr)
 library(magick) # Required for cowplot to plot images
 library(ggthemr)
 library(ggrepel)
-library(latex2exp)
 source("preston.R")
 source("plot_colours.R")
 ##################
@@ -732,10 +731,10 @@ p <- preston_df %>% ggplot() +
                           # breaks = scales::trans_breaks("log10", function(x) 10^x),
                           # labels = scales::trans_format("log10", scales::math_format(10^.x)))+
   scale_fill_manual("", values = c("grey50"))+
-  scale_x_log10("Species richness",
+  scale_x_log10("Area",
                 breaks = scales::trans_breaks("log10", function(x) 10^x),
                 labels = scales::trans_format("log10", scales::math_format(10^.x)))+
-  scale_y_log10("Area",
+  scale_y_log10("Species richness",
                 breaks = scales::trans_breaks("log10", function(x) 10^x),
                 labels = scales::trans_format("log10", scales::math_format(10^.x)))+
   theme_classic()
